064ea1408d55fa139aa41f5dffc9b47feed699a9
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-18 21:30:13 +0200

    added buyPetrolTest

--- src/test/java/com/JStudio/Statoil/CustomerTest.java
+++ src/test/java/com/JStudio/Statoil/CustomerTest.java
@@ -1,10 +1,39 @@
 package com.JStudio.Statoil;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
 
+	Station station;
+	Petrol petrol;
+	Container container;
+	Customer customer;
+	double containerSize = 100;
 
+	@Before
+	public void setUp(){
+
+		station = new Station();
+		petrol = new Petrol(PetrolType.PB95);
+		container = new Container(PetrolType.PB95);
+		customer = new Customer();
+
+		container.addPetrol(containerSize);
+		station.addContainer(container);
+	}
+
+	@Test
+	public void buyPetrolTest(){
+
+		double litersOfPetrol = 40;
+		double oldAmountOfPetrolInCar = customer.getCar().getCurrentCapacity();
+		customer.buyPetrol(PetrolType.PB95, litersOfPetrol);
+
+		assertThat(station.getContainerList().get(0).getCurrentCapacity(), is(containerSize - litersOfPetrol));
+		assertTrue(customer.getCar().getCurrentCapacity() > oldAmountOfPetrolInCar);
+	}
 }
