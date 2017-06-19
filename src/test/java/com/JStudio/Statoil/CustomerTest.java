9138b6dfc43687a76ccd1abad86331e9109449d8
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-19 21:42:12 +0200

    added buyMorePetrolThanCarCapacity

--- src/test/java/com/JStudio/Statoil/CustomerTest.java
+++ src/test/java/com/JStudio/Statoil/CustomerTest.java
@@ -12,7 +12,7 @@ public class CustomerTest {
 	Station station;
 	Container container;
 	Customer customer;
-	double containerSize = 100;
+	double containerSize = 1000;
 
 	@Before
 	public void setUp() {
@@ -44,5 +44,17 @@ public class CustomerTest {
 		customer.buyPetrol(PetrolType.PB95, litersOfPetrol);
 
 		assertThat(customer.getTankCost(), is(litersOfPetrol * new PetrolRepository().getPrice(PetrolType.PB95)));
+	}
+
+	@Test
+	public void buyMorePetrolThanCarCapacity(){
+		double litersOfPetrol = 200;
+		double oldAmountOfPetrolInCar = customer.getCar().getCurrentPetrolCapacity();
+		customer.buyPetrol(PetrolType.PB95, litersOfPetrol);
+		double difference = customer.getCar().getCurrentPetrolCapacity() - oldAmountOfPetrolInCar;
+
+		assertThat(station.getContainerList().get(0).getCurrentCapacity(), is(containerSize - difference));
+		assertTrue(customer.getCar().getCurrentPetrolCapacity() > oldAmountOfPetrolInCar);
+		assertThat(customer.getTankCost(), is(difference * new PetrolRepository().getPrice(PetrolType.PB95)));
 	}
 }
